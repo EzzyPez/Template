c5be0a2c3b4e6925c8e828e41b10c818a897148e
EzzyPez <draganovicdragan97@gmail.com>
2018-10-14 21:33:53 +0200

    password rehash added

--- php/login.php
+++ php/login.php
@@ -1,39 +1,35 @@
 <?php
 require('connect.php');
 session_start();
-// If form submitted, insert values into the database.
+
 if (isset($_POST['username'])){
-        // removes backslashes
 	$username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
 	$username = mysqli_real_escape_string($con,$username);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);	
-    }
 
-    $pass_query = "SELECT password FROM `user_data` WHERE username = '$username';";
-    $hash_query = mysqli_query($con, $pass_query);
-    $result = "";
+	$pass_query = "SELECT password FROM `user_data` WHERE username = '$username';";
+	$hash_query = mysqli_query($con, $pass_query);
+	$result = "";
 
-    if(mysqli_num_rows($hash_query) > 0){
-	    while($row = mysqli_fetch_assoc($hash_query)){
-		$result = $result . $row["password"];
-	    }
-    }
-
-    if(password_verify($password, $result)){
-    	//Checking is user existing in the database or not
-   	$query = "SELECT * FROM `user_data` WHERE username='$username'AND password='".md5($password)."'";
-	$result = mysqli_query($con,$query) or die(mysql_error());
-	$rows = mysqli_num_rows($result);
-        if($rows==1){
-	    $_SESSION['username'] = $username;
-            // Redirect user to index.php
-	    header("Location: ../index.php");
-	} 
+    	if(mysqli_num_rows($hash_query) > 0){
+		while($row = mysqli_fetch_assoc($hash_query)){
+			$result = $result . $row["password"];
+	    	}
+    	}	    				
+	if(password_verify($password, $result)){
+		if(password_needs_rehash($result, PASSWORD_DEFAULT)){
+			$new_hash = password_hash($password, PASSWORD_DEFAULT);
+			$new_hash_query = mysqli_query("UPDATE `user_data` SET password = '$new_hash' WHERE username = '$username';");
+		}
+		$_SESSION['username'] = $username;
+		//============================================================================
+		header("Location: ../dashboard.php");  
+		//============================================================================    		
 	}else{
 		echo "<div class='form'>
 		<h3>Username/password is incorrect.</h3>
 		<br/>Click here to <a href='../login.html'>Login</a></div>";
 	}
+}
 ?>
